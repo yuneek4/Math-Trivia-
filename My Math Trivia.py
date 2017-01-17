cb7a02df785c7269b3d9ca70a8bd98812d05069f
yuneek4 <yuneek4@gmail.com>
2017-01-17 14:15:56 -0500

    10 questions for man trivia

---
+++ My Math Trivia.py
@@ -0,0 +1,150 @@
+# Math Trivia
+print("This is a math trivia to test you knowledge in math")
+name = input("Enter your name: ")
+print("\nHello my friend I hope you have remembered your math from the past years. Are you ready?\n")
+print("Here, I will be asking you 10 quick and easy questions and give you three choices in order to answer it")
+print("Please select the right choice by pressing A, B or C")
+
+print("-------------------------------------------------------------------------")
+
+# set quiz score to 0
+
+score = 0
+score = int(score) # convert 0 into a number so we can add scores
+
+# question 1
+print("Question 1: What comes up first in PEMDAS?")
+print("A.Multiplication")
+print("B.Parenthesis")
+print("C.Exponents")
+print("")
+Q1answer = "B" # right answer to question 1
+Q1response = input("your answer: ")
+if Q1response == "B" or Q1response == "b":
+    print("great job " + Q1response + " is correct!")
+    score = score + 1
+else:
+    print("sorry, that answer is incorrect")
+print("Your current score is " + str(score) +  " out of 10 ")
+
+# question 2
+print("Question 2: What is the square root of 144?")
+print("A.10")
+print("B.12")
+print("C.15")
+print("")
+Q1answer = "B" # right answer to question 1
+Q1response = input("your answer: ")
+if Q1response == "B" or Q1response == "b":
+    print("great job " + Q1response + " is correct!")
+    score = score + 1
+else:
+    print("sorry, that answer is incorrect")
+print("Your current score is " + str(score) +  " out of 10 ")
+
+# question 3
+print("Question 1: What does (7+2)2  /4+2-1?")
+print("A.0")
+print("B.10")
+print("C.26")
+print("")
+Q1answer = "C" # right answer to question 1
+Q1response = input("your answer: ")
+if Q1response == "C" or Q1response == "c":
+    print("great job " + Q1response + " is correct!")
+    score = score + 1
+else:
+    print("sorry, that answer is incorrect")
+print("Your current score is " + str(score) +  " out of 10 ")
+
+# question 4
+print("Question 1: What is the square root of 256?")
+print("A.16")
+print("B.15.5")
+print("C.15")
+print("")
+Q1answer = "A" # right answer to question 1
+Q1response = input("your answer: ")
+if Q1response == "A" or Q1response == "a":
+    print("great job " + Q1response + " is correct!")
+    score = score + 1
+else:
+    print("sorry, that answer is incorrect")
+print("Your current score is " + str(score) +  " out of 10 ")
+
+# question 5
+print("Question 1: What is the area of a square when given the side 2?")
+print("A.4")
+print("B.7")
+print("C.3.5")
+print("")
+Q1answer = "A" # right answer to question 1
+Q1response = input("your answer: ")
+if Q1response == "A" or Q1response == "a":
+    print("great job " + Q1response + " is correct!")
+    score = score + 1
+else:
+    print("sorry, that answer is incorrect")
+print("Your current score is " + str(score) +  " out of 10 ")
+
+# question 6
+print("Question 1: When given the area 400, what would one side of the square be?")
+print("A.14")
+print("B.20")
+print("C.16")
+print("")
+Q1answer = "B" # right answer to question 1
+Q1response = input("your answer: ")
+if Q1response == "B" or Q1response == "b":
+    print("great job " + Q1response + " is correct!")
+    score = score + 1
+else:
+    print("sorry, that answer is incorrect")
+print("Your current score is " + str(score) +  " out of 10 ")
+
+# question 7
+print("Question 1: What shape formula is related to (b*h)/2?")
+print("A.Triangle")
+print("B.Circle")
+print("C.Parallelogram")
+print("")
+Q1answer = "A" # right answer to question 1
+Q1response = input("your answer: ")
+if Q1response == "A" or Q1response == "a":
+    print("great job " + Q1response + " is correct!")
+    score = score + 1
+else:
+    print("sorry, that answer is incorrect")
+print("Your current score is " + str(score) +  " out of 10 ")
+
+# question 8
+print("Question 1: The data values are: 2,5,7,9. What is the mean?")
+print("A.10")
+print("B.5.75")
+print("C.5.74125")
+print("")
+Q1answer = "B" # right answer to question 1
+Q1response = input("your answer: ")
+if Q1response == "B" or Q1response == "b":
+    print("great job " + Q1response + " is correct!")
+    score = score + 1
+else:
+    print("sorry, that answer is incorrect")
+print("Your current score is " + str(score) +  " out of 10 ")
+
+# question 8a
+print("Question 1: Based on the data above, What would the median be?")
+print("A.6")
+print("B.5.91")
+print("C.7")
+print("")
+Q1answer = "A" # right answer to question 1
+Q1response = input("your answer: ")
+if Q1response == "A" or Q1response == "a":
+    print("great job " + Q1response + " is correct!")
+    score = score + 1
+else:
+    print("sorry, that answer is incorrect")
+print("Your current score is " + str(score) +  " out of 10 ")
+
+
